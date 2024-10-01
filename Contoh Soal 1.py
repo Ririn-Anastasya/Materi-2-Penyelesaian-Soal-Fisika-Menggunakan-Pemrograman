6666e770e5c748caedd40c00eb81408ca4234ee0
Ririn-Anastasya <rrnanastasya96@gmail.com>
2024-10-02 00:13:14 +0700

    Add files via upload

---
+++ Contoh Soal 1.py
@@ -0,0 +1,7 @@
+# w = mg
+w = 25.6  #N
+g = 9.8  #N/Kg
+
+# m = w/g
+m = w / g
+print(" Massa : ",m, "Kg")
---
+++ Tugas 1A.py
@@ -0,0 +1,7 @@
+n = 1.50 
+R1 = 22.0 
+R2 = 17.5 
+
+f_inv = (n - 1)*(1/R1 + 1/R2)
+f = 1/f_inv 
+print("Jarak Fokus Lensa (f) = {:.2f} cm".format(f))
